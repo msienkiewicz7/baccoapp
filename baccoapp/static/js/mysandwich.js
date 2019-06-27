a146f1b78bf3c35ecbb418b09bea0a2a3af2c570
Michal Sienkiewicz <s73271@beuth-hochschule.de>
2019-06-27 21:31:42 +0200

    big create update: select events + create seite css

--- baccoapp/static/js/mysandwich.js
+++ baccoapp/static/js/mysandwich.js
@@ -2,64 +2,120 @@ $(document).ready(function() {
 
   console.log("jQuery magic starting...")
 
-  // $("#id_ingredients option").each(function() {
-  //
-  //   let option = $(this)
-  //   let data = option.attr("data").split(",")
-  //   let ingredient_name = data[0]
-  //   let ingredient_price = data[1]
-  //   let ingredient_type = data[2]
-  //   let img_small = data[3]
-  //   let img_large = data[4]
-  //
-  //   option.html(ingredient_name)
-  //   option.attr("price", ingredient_price)
-  //   option.attr("type", ingredient_type)
-  //   option.attr("img_small", img_small)
-  //   option.attr("img_large", img_large)
-  //
-  //   option.css("background-image", `url(/${img_small})`)
-  //   // option.css("background-size", "5%")
-  //   // console.log(img_small);
-  // });
+// Update price on changed selection
+//   $("#id_ingredients").on("change", function(event){
+//
+//     var price = 0;
+//     var all_selected_ingredient_ids = "";
+//     var selection = $(this).find(":selected");
+//     selection.each(function(){
+//       price += Number($(this).attr("price"));
+//
+//       if (all_selected_ingredient_ids != ""){
+//         all_selected_ingredient_ids += ",";
+//       }
+//       all_selected_ingredient_ids += $(this).attr("value");
+//     });
+//     // console.log(price);
+//     // alert(all_selected_ingredient_ids);
+//
+//     $("#id_price").attr("value", price);
+//     $("#id_all_selected_ingredient_ids").attr("value", all_selected_ingredient_ids);
+//   });
+  $("#id_price").attr("readonly", true)
 
-  // $("#id_ingredients").on("change", function(event) {
+// // Allow multiple selection without holding cmd
+//   $('#id_ingredients option').mousedown(function(e) {
+//       e.preventDefault();
+//       $(this).prop('selected', !$(this).prop('selected'));
+//       return false;
+//   });
+
+
+// Load preview images on chenged selection
+  // $("#id_ingredients").on("change", function(event){
+  //   console.log("Change event!");
   //
-  //   let price = 0;
-  //   let selection = $(this).find(":selected");
+  //   $(".preview img").remove();
+  //
+  //   var selection = $(this).find(":selected");
+  //   console.log(selection);
   //   selection.each(function(){
-  //     price += Number($(this).attr("price"));
-  //   });
-  //   console.log(price);
+  //     let img_src = $(this).attr("img");
+  //     let img = $("<img></img>").attr("src", img_src);
+  //     $(".preview").append(img);
   //
-  //   $("#id_price").attr("value", price)
+  //   });
+  //
   //
   // });
 
-  $("#id_ingredients").on("change", function(event){
 
-    var price = 0;
-    var all_selected_ingredient_ids = "";
-    var selection = $(this).find(":selected");
-    selection.each(function(){
-      price += Number($(this).attr("price"));
+  $("#id_ingredients").mousedown(function(e){
+    // console.log("Mouse down event!");
+      e.preventDefault();
+// Allow miltiple selections without holding extra key
+      var select = this;
+      var scroll = select .scrollTop;
 
-      if (all_selected_ingredient_ids != ""){
-        all_selected_ingredient_ids += ",";
+      e.target.selected = !e.target.selected;
+
+      setTimeout(function(){select.scrollTop = scroll;}, 0);
+
+      $(select ).focus();
+
+// Dynamicly change the price
+      var price = 0;
+      var all_selected_ingredient_ids = "";
+      var selection = $(this).find(":selected");
+      selection.each(function(){
+        price += Number($(this).attr("price"));
+
+        if (all_selected_ingredient_ids != ""){
+          all_selected_ingredient_ids += ",";
+        }
+        all_selected_ingredient_ids += $(this).attr("value");
+      });
+      price = price.toFixed(2);
+      $("#id_price").attr("value", price);
+      $("#id_all_selected_ingredient_ids").attr("value", all_selected_ingredient_ids);
+
+// Load preview images
+      $(".preview img").remove();
+      // var selection = $(this).find(":selected");
+
+      let containsBread = false;
+      let bottom_bread_src;
+      let img;
+      selection.each(function(){
+        let ingr_type = $(this).attr("type");
+        if (ingr_type == "BREAD") {
+          containsBread = true;
+          let top_bread_src = $(this).attr("img_small");
+          bottom_bread_src = $(this).attr("img_large");
+          img = $("<img></img>").attr("src", top_bread_src);
+          $(".sandwich_images").prepend(img);
+        } else {
+          let img_src = $(this).attr("img_large");
+          img = $("<img></img>").attr("src", img_src);
+          $(".sandwich_images").append(img);
+        }
+      });
+
+      if (containsBread) {
+        img = $("<img></img>").attr("src", bottom_bread_src);
+        $(".sandwich_images").append(img);
       }
-      all_selected_ingredient_ids += $(this).attr("value");
-    });
-    // console.log(price);
-    // alert(all_selected_ingredient_ids);
+  });
 
-    $("#id_price").attr("value", price);
-    $("#id_all_selected_ingredient_ids").attr("value", all_selected_ingredient_ids);
+  $("#reset").click(function(){
+    // $("option:selected").prop("selected", false)
 
+    $("#id_ingredients option:selected").prop("selected", false)
   });
 
 
 
-  // $("#id_pub_date").attr("hidden", true)
-  $("#id_price").attr("readonly", true)
+
 
 });
